511a8bbe113a1a862958c77f2529b461d8e5d203
alonzathompson <thompson0005@gmail.com>
2018-08-25 16:20:15 -0400

    edits for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/https://alonzathompson.github.io/vueSongTracker/',
       name: 'root',
       component: HelloWorld
     },
